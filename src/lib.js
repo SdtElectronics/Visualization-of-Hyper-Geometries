cd76f7d81be4ab7916c3968ed9c11d24dc1b73e9
Excelsior <null@std.uestc.edu.com>
2019-06-17 23:55:42 +0800

    nD Vector debugging

--- src/lib.js
+++ src/lib.js
@@ -71,17 +71,20 @@ geomBase.updateLine = (dots, line, ord = null) => {
 geomBase.updateArr = (dot, geometry, color = null) => {
 	const ori = new THREE.Vector3(...dot[0]);
 	const vec = new THREE.Vector3(...dot[1]);
-	geometry.position.set(ori);
-	geometry.setDirection(vec.normalize());
-	geometry.setLength(vec.length());
+	geometry.position.copy(ori);
+	const len = vec.length();
+	if(len){
+		geometry.setLength(len , 0.2 * len, 6);
+		geometry.setDirection(vec.normalize());
+	}
 	if(!!color)
-		geometry.setColor(color);
+		geometry.setColor(new THREE.Color(color));
 };
 
 geomBase.update = (angularSpeed = 0.1) => {
-	let time = (new Date()).getTime();
-    let timeDiff = time - geomBase.lastTime;
-	let angleChange = angularSpeed * timeDiff * 2 * Math.PI / 750;
+	const time = (new Date()).getTime(),
+    	  timeDiff = time - geomBase.lastTime,
+		  angleChange = angularSpeed * timeDiff * 2 * Math.PI / 750;
 	geomBase.geomLst.forEach(e => {
 		if(!!(e.spin[0]))
 			e.update(angleChange, null, e.faceOrd);
@@ -315,6 +318,7 @@ class hilbertCurve extends geomBase{
 		this.Vecs = this.baseVec();
 		this.movVec();
 		this.initVec();
+
 		scene.add(this.line);
 	}
 	baseVec(){
@@ -358,7 +362,9 @@ class ndArr extends geomBase{
 		this.spin = spin;
 		this.projType = projType;
 		this.projVec(vec);
+		this.initArr();
 
+		this.arrs.forEach(e => scene.add(e));
 	}
 
 	projVec(type){
@@ -368,15 +374,17 @@ class ndArr extends geomBase{
 				return pre > curr ? pre : curr;
 			});
 			this.proj = this.Vecs.map(dot => {
-				dot[0] = dot[0].slice(0,3);
-				dot[1] = geomBase.proj(dot[1], dist);
-				return dot;
+				const ret = [];
+				ret[0] = dot[0].slice(0,3);
+				ret[1] = geomBase.proj(dot[1], dist);
+				return ret;
 			});
 		}else{
 			this.proj = this.Vecs.map(dot => {
-				dot[0] = dot[0].slice(0,3);
-				dot[1] = dot[1].slice(0,3);
-				return dot;
+				const ret = [];
+				ret[0] = dot[0].slice(0,3);
+				ret[1] = dot[1].slice(0,3);
+				return ret;
 			});
 		}
 	}
@@ -384,7 +392,7 @@ class ndArr extends geomBase{
 	initArr(){
 		this.projVec(this.projType);
 		this.arrs = this.proj.map((vec, i) => {
-			return geomBase.drawArr(this.proj, this.Vecs[i][1].concat().pop());
+			return geomBase.drawArr(vec, this.Vecs[i][1].concat().pop());
 		});
 	}
 
@@ -422,46 +430,54 @@ class ndArr extends geomBase{
 ndArr.fromFunc = (expr, initVal, initPara, steps, stepLen = 1, 
 												  dim = null, 
 												  spin = null, 
-												  projType = true, 
+												  projType = false, 
 												  specColor = false) => {
 	if(!specColor){
 		dim = expr.length;
 		const rainbow = new Rainbow();
-		rainbow.setSpectrum("blue", "red");
-		rainbow.setNumberRange(0, Math.sqrt((steps * stepLen / 2) ** 2));
-		expr.push((dims => {
-			this.colourAt(math.norm(dims));
+		rainbow.setSpectrum("blue", "green", "red");
+		rainbow.setNumberRange(0, (steps * stepLen / 2) * Math.sqrt(2));
+		expr.push((function(dims) {
+			dims = dims.concat().pop();
+			return parseInt(`0x${this.colourAt(math.norm(dims))}`, 16) ;
 		}).bind(rainbow));
 	}
-	const func = new ndVecFunc(expr, initVal, initPara, steps, stepLen = 1, dim);
-	const ret =  new ndArr(dim, func, spin, projType);
-	ret.func = func;
+	const func = new ndVecFunc(expr, initVal, initPara, steps, stepLen, dim, true);
+	func.vecs.splice(1, 1);
+	console.log(func);
+	const ret =  new ndArr(dim, func.vecs, spin, projType);
 	return ret;
 }
 
 
 
-class ndVecFunc extends Array{
-	constructor(expr, initVal, initPara, steps, stepLen = 1, dim = null){
-		super();
+class ndVecFunc{
+	constructor(expr, initVal, initPara, steps, stepLen = 1, dim = null, color = false){
+		this.vecs = [];
 		this.expr = expr;
 		this.initVal = initVal;
 		this.steps = steps;
 		this.stepLen = stepLen;
 		this.dim = !!dim ? dim : expr.length;
-		permute(Array(this.dim).fill(Array(steps).fill(0).map(() => initVal += stepLen))).forEach(val => {
+		this.color = color;
+		
+		permute(Array(this.dim).fill(Array(steps).fill(0).map(() => initVal += stepLen))).forEach((val, ind) => {
 			const vect = [];
-			expr.forEach((exp, i) => vect.push(exp(val[i], ...initPara)));
-			this.push([val, vect]);
+			if(color)
+				val = val.concat([val]);
+			expr.forEach((exp, i) => {
+					vect.push(exp(val, ...initPara));
+				});
+			this.vecs.push([val, vect]);
 		})
 	}
 
 	update(para){
 		permute(Array(this.expr.length).fill(Array(this.steps).fill(0).map(() => 
 			this.initVal += this.stepLen))).forEach((val, index) => {
-			expr.forEach((exp, i) => this[index][1][i] = exp(val[i], ...para));
+			expr.forEach((exp, i) => this.vecs[index][1][i] = exp(val[i], ...para));
 		})
-		return this;
+		return this.vecs;
 	}
 }
 
@@ -469,6 +485,8 @@ function toRad (deg){
 	return deg / 180 * Math.PI;
 }
 
+
+//following codes are runtime libs mainly from stackOverflow
 function permute(input) {
   var out = [];
 
@@ -783,152 +801,8 @@ function ColourGradient()
 	// Extended list of CSS colornames s taken from
 	// http://www.w3.org/TR/css3-color/#svg-color
 	var colourNames = {
-		aliceblue: "F0F8FF",
-		antiquewhite: "FAEBD7",
-		aqua: "00FFFF",
-		aquamarine: "7FFFD4",
-		azure: "F0FFFF",
-		beige: "F5F5DC",
-		bisque: "FFE4C4",
-		black: "000000",
-		blanchedalmond: "FFEBCD",
 		blue: "0000FF",
-		blueviolet: "8A2BE2",
-		brown: "A52A2A",
-		burlywood: "DEB887",
-		cadetblue: "5F9EA0",
-		chartreuse: "7FFF00",
-		chocolate: "D2691E",
-		coral: "FF7F50",
-		cornflowerblue: "6495ED",
-		cornsilk: "FFF8DC",
-		crimson: "DC143C",
-		cyan: "00FFFF",
-		darkblue: "00008B",
-		darkcyan: "008B8B",
-		darkgoldenrod: "B8860B",
-		darkgray: "A9A9A9",
-		darkgreen: "006400",
-		darkgrey: "A9A9A9",
-		darkkhaki: "BDB76B",
-		darkmagenta: "8B008B",
-		darkolivegreen: "556B2F",
-		darkorange: "FF8C00",
-		darkorchid: "9932CC",
-		darkred: "8B0000",
-		darksalmon: "E9967A",
-		darkseagreen: "8FBC8F",
-		darkslateblue: "483D8B",
-		darkslategray: "2F4F4F",
-		darkslategrey: "2F4F4F",
-		darkturquoise: "00CED1",
-		darkviolet: "9400D3",
-		deeppink: "FF1493",
-		deepskyblue: "00BFFF",
-		dimgray: "696969",
-		dimgrey: "696969",
-		dodgerblue: "1E90FF",
-		firebrick: "B22222",
-		floralwhite: "FFFAF0",
-		forestgreen: "228B22",
-		fuchsia: "FF00FF",
-		gainsboro: "DCDCDC",
-		ghostwhite: "F8F8FF",
-		gold: "FFD700",
-		goldenrod: "DAA520",
-		gray: "808080",
-		green: "008000",
-		greenyellow: "ADFF2F",
-		grey: "808080",
-		honeydew: "F0FFF0",
-		hotpink: "FF69B4",
-		indianred: "CD5C5C",
-		indigo: "4B0082",
-		ivory: "FFFFF0",
-		khaki: "F0E68C",
-		lavender: "E6E6FA",
-		lavenderblush: "FFF0F5",
-		lawngreen: "7CFC00",
-		lemonchiffon: "FFFACD",
-		lightblue: "ADD8E6",
-		lightcoral: "F08080",
-		lightcyan: "E0FFFF",
-		lightgoldenrodyellow: "FAFAD2",
-		lightgray: "D3D3D3",
-		lightgreen: "90EE90",
-		lightgrey: "D3D3D3",
-		lightpink: "FFB6C1",
-		lightsalmon: "FFA07A",
-		lightseagreen: "20B2AA",
-		lightskyblue: "87CEFA",
-		lightslategray: "778899",
-		lightslategrey: "778899",
-		lightsteelblue: "B0C4DE",
-		lightyellow: "FFFFE0",
-		lime: "00FF00",
-		limegreen: "32CD32",
-		linen: "FAF0E6",
-		magenta: "FF00FF",
-		maroon: "800000",
-		mediumaquamarine: "66CDAA",
-		mediumblue: "0000CD",
-		mediumorchid: "BA55D3",
-		mediumpurple: "9370DB",
-		mediumseagreen: "3CB371",
-		mediumslateblue: "7B68EE",
-		mediumspringgreen: "00FA9A",
-		mediumturquoise: "48D1CC",
-		mediumvioletred: "C71585",
-		midnightblue: "191970",
-		mintcream: "F5FFFA",
-		mistyrose: "FFE4E1",
-		moccasin: "FFE4B5",
-		navajowhite: "FFDEAD",
-		navy: "000080",
-		oldlace: "FDF5E6",
-		olive: "808000",
-		olivedrab: "6B8E23",
-		orange: "FFA500",
-		orangered: "FF4500",
-		orchid: "DA70D6",
-		palegoldenrod: "EEE8AA",
-		palegreen: "98FB98",
-		paleturquoise: "AFEEEE",
-		palevioletred: "DB7093",
-		papayawhip: "FFEFD5",
-		peachpuff: "FFDAB9",
-		peru: "CD853F",
-		pink: "FFC0CB",
-		plum: "DDA0DD",
-		powderblue: "B0E0E6",
-		purple: "800080",
 		red: "FF0000",
-		rosybrown: "BC8F8F",
-		royalblue: "4169E1",
-		saddlebrown: "8B4513",
-		salmon: "FA8072",
-		sandybrown: "F4A460",
-		seagreen: "2E8B57",
-		seashell: "FFF5EE",
-		sienna: "A0522D",
-		silver: "C0C0C0",
-		skyblue: "87CEEB",
-		slateblue: "6A5ACD",
-		slategray: "708090",
-		slategrey: "708090",
-		snow: "FFFAFA",
-		springgreen: "00FF7F",
-		steelblue: "4682B4",
-		tan: "D2B48C",
-		teal: "008080",
-		thistle: "D8BFD8",
-		tomato: "FF6347",
-		turquoise: "40E0D0",
-		violet: "EE82EE",
-		wheat: "F5DEB3",
-		white: "FFFFFF",
-		whitesmoke: "F5F5F5",
-		yellow: "FFFF00",
-		yellowgreen: "9ACD32"
+		green: "008000"
 	}
 }
